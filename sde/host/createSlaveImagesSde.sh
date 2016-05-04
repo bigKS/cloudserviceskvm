00f20a1465016fd79ab119b53f3e796c1cff56d3
root <root@batman1.sandvine.rocks>
2016-05-04 10:14:05 -0400

    add SDE bootstrap updates

---
+++ sde/host/createSlaveImagesSde.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+scriptName=$(basename "$0")
+
+## This script takes a master PTS C.S. QCOW and makes slave ones for every PTS to be run on this blades
+
+## Arguments
+# Arg1:  Full path to master images
+# Arg2:  Full path to location where the slave images should be created
+# Arg3:  The number 1 or number 2 
+# Arg4:  The number of PTSes to create
+numArgs=4
+
+# Each PTS image name will be "dpiY-ptsX" where Y is 1 or 2 (denoting dpi blade number) and X is PTS number (starting with number 1)
+
+function printUsageExit ()
+{
+	{
+	echo
+	echo "Usage:"
+	echo "> $scriptName <path master image> <directory path slave images> <number 1 or 2 for dpi blade> <number of PTS images to create>"
+	echo
+	echo "Synopsis:"
+	echo "Will create PTS qcow disk images from a master qcow with the filenames dpiY-ptsX.qcow2c" 
+	echo "where Y is 1 or 2 to denote blade, and X is a number starting from 1 and going upto the number of PTS images to create"
+	echo 
+	} >&2
+
+	exit 1
+}
+
+
+function printErrorExit () {
+    echo -e "\nERROR: $@" >&2
+	printUsageExit
+}
+
+# We want this to be run as root
+(( $(id -u) == 0 )) || printErrorExit "This script must be run as root."
+
+# Number of arguments check..
+[[ $# == $numArgs ]] || printErrorExit "Number of arguments to script must be <$numArgs> but only <$#> were supplied"
+
+masterImagePath=$1
+slaveImageDirectory=$2
+dpiNum=$3
+numPts=$4
+
+# Error check on master image argument
+[[ -e $masterImagePath ]] || printErrorExit "The path to master image <$masterImagePath> supplied does not seem to correlate to a file."
+[[ $(basename $masterImagePath) =~ .*pts.*cs.*\.qcow2c ]]  || printErrorExit "The master image <$masterImagePath> does not see to have reasonable file name of a pts c.s. qcow image."
+
+# Error check on slave image argument
+[[ -d $slaveImageDirectory ]] || printErrorExit "Directory path to store slave images suppled <$slaveImageDirectory> does not seem to exist."
+ls "$slaveImageDirectory"/dpi*-pts*.qcow2c &> /dev/null  &&
+	printErrorExit "Directory path to store slave images already has pts slave qcows in it. Please rectify this."
+
+# Error check on dpi number
+[[ $dpiNum == "1" ]] || [[ $dpiNum == "2" ]] || printErrorExit "The third argument should be number 1 or 2 to denote dpi blade. You supplied <$dpiNum>"
+
+# Error check on num pts
+[[ $numPts =~ [1-9][0-9]* ]] || printErrorExit "Fourth argument should be a positive integer to denote the number of PTS slave images to create. You supplied <$numPts>"
+
+echo "You have supplied what seem like reasonable arguments to this script, will attempt to create slave pts images in location specified..."
+echo 
+
+# Create the pts slave images
+for iter in $(seq $numPts); do
+
+	ptsImageFilename="dpi${dpiNum}-pts${iter}.qcow2c"
+
+	echo "Attempting to create pts slave image <$ptsImageFilename>...	"
+
+	set -x
+	qemu-img convert -f qcow2 -O qcow2 "$masterImagePath" "$slaveImageDirectory/$ptsImageFilename"
+	status=$?
+	set +x
+
+	(( $? == 0 )) || printErrorExit "Creating of <iter>-th pts slave image seems to have failed. Please investiagte"
+
+	echo "Slave image creation seems to have succeeded."
+done
+
+echo "It seems that all slave images have been created successfully.  Please do a sanity look over of <$slaveImageDirectory> before deploying the PTSes..."
